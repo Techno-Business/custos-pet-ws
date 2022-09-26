c410c21256aeb80b8addb859375144434f7e744b
Lucas Gabriel <lucasogjc@gmail.com>
2022-09-26 17:58:50 -0300

    feat: adds password encryption on sign up use case

--- src/modules/owner/useCases/SignUp/OwnerSignUpUseCase.ts
+++ src/modules/owner/useCases/SignUp/OwnerSignUpUseCase.ts
@@ -1,6 +1,7 @@
 import { OwnerSignUpDto } from "./OwnerSignUpDto";
 import { OwnerModel } from "../../owner.model";
 import { IOwnerRepository } from "../../owner.repository";
+import bcrypt from "bcrypt";
 
 export class OwnerSignUpUseCase {
     constructor(
@@ -9,18 +10,19 @@ export class OwnerSignUpUseCase {
     }
 
     async execute(data: OwnerSignUpDto): Promise<OwnerModel> {
-
         const ownerExists = await this.ownerRepository.existsByEmail(data.email);
 
         if (ownerExists) {
             throw new Error('Owner already exists.');
         }
 
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
         const owner = new OwnerModel(
             data.firstName,
             data.lastName,
             data.email,
-            data.password,
+            hashedPassword,
         )
 
         return await this.ownerRepository.save(owner);
