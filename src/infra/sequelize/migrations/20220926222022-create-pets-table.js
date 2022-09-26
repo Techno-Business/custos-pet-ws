3888181aa78800491071632d09865a2ff1467ad0
Lucas Gabriel <lucasogjc@gmail.com>
2022-09-26 19:50:53 -0300

    feat: adds pet migration and sequelize model

---
+++ src/infra/sequelize/migrations/20220926222022-create-pets-table.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('pets', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      photo: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1, 3]
+        }
+      },
+      sex: {
+        type: Sequelize.STRING(6),
+        allowNull: false,
+      },
+      species: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      breed: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      owner_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'owners',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pets');
+  }
+};
---
+++ src/infra/sequelize/models/Pet.ts
@@ -0,0 +1,67 @@
+import { db } from "../../db";
+import { DataTypes } from "sequelize";
+import Owner from "./Owner";
+
+const Pet = db.define('pets', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    photo: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            len: [1, 3]
+        }
+    },
+    sex: {
+        type: DataTypes.STRING(6),
+        allowNull: false,
+    },
+    species: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    breed: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    owner_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: 'owners',
+            key: 'id',
+        },
+    },
+    created_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+    },
+    updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+    }
+},{
+    timestamps: true,
+    underscored: true,
+    tableName: 'pets',
+    indexes: [
+        { unique: true, fields: ['email'] },
+    ]
+});
+
+Owner.hasMany(Pet);
+Pet.belongsTo(Owner);
+
+export default Pet;
