e0b5cf688ce92d57848d0cb1d4cfa5b248c943ed
Lucas Gabriel <lucasogjc@gmail.com>
2022-09-28 22:15:24 -0300

    feat: adds cost details model and migration

---
+++ src/infra/sequelize/models/Details.ts
@@ -0,0 +1,35 @@
+import { db } from "../../db";
+import { DataTypes } from "sequelize";
+
+const Details = db.define('details', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+    },
+    service_type: {
+        type: DataTypes.CHAR(21),
+        allowNull: true,
+    },
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    weight: {
+        type: DataTypes.REAL,
+        allowNull: true,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+},{
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    underscored: true,
+    tableName: 'pets',
+});
+
+export default Details;
