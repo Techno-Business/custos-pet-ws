ad2688504757f1fc98292e134dd399592ca06bb4
Lucas Gabriel <lucasogjc@gmail.com>
2022-10-28 23:37:46 -0300

    feat: adds address, diary and pet diary table migrations

---
+++ src/infra/sequelize/migrations/20221029014415-create-addresses-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      postal_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      neighbourhood: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('addresses')
+  }
+};
---
+++ src/infra/sequelize/migrations/20221029020602-create-diaries-table.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('diaries', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      address_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'addresses',
+          key: 'id',
+        }
+      },
+      pet_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'pets',
+          key: 'id',
+        }},
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('diaries')
+  }
+};
